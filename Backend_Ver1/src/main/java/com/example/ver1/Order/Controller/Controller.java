9c93268a16acb104b65f5bb7af6d48a6616d9650
Luyen Dao Dinh <91018384+henrydao15@users.noreply.github.com>
2022-08-17 14:34:08 +0700

    order controller update

--- Backend_Ver1/src/main/java/com/example/ver1/Order/Controller/Controller.java
+++ Backend_Ver1/src/main/java/com/example/ver1/Order/Controller/Controller.java
@@ -4,9 +4,9 @@ import com.example.ver1.Order.service.OrderService;
 import com.example.ver1.Stations.model.Stations;
 import com.example.ver1.Stations.service.StationsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/orders")
@@ -18,13 +18,25 @@ public class Controller {
     List<Order> getAllOrderList(){
         return orderService.getAllOrders();
     }
-
-    @GetMapping(path = "/user/{cardNum}")
-    List<Order> getAllOrderListByUserID(@PathVariable String cardNum){
-        return orderService.getAllOrders().stream().filter(s -> s.getCard().getCardNum().equals(cardNum)).collect(Collectors.toList());
+    @GetMapping(path = {"/admin/page/{pageNo}", "/admin/page"})
+    Page<Order> getAllOrderListByPage(@PathVariable(required = false) Integer pageNo){
+        int pageSize = 20;
+        if(pageNo != null) {
+            return orderService.getAllOrderByPage(pageNo, pageSize);
+        }
+        return orderService.getAllOrderByPage(1, pageSize);
     }
 
-    @GetMapping(path = "{id}")
+    @GetMapping(path = {"/user/{cardNum}/{pageNo}", "/user/{cardNum}"})
+    Page<Order> getAllOrderListByUserID(@PathVariable String cardNum, @PathVariable Integer pageNo){
+        int pageSize = 20;
+        if(pageNo != null) {
+            return orderService.getAllOrderByCardNumber(cardNum, pageNo, pageSize);
+        }
+        return orderService.getAllOrderByCardNumber(cardNum, 1, pageSize);
+    }
+
+    @GetMapping(path = {"/admin/{id}", "/user/{id}"})
     Order getOrderById(@PathVariable long id){
         return orderService.getOrderById(id);
     }
@@ -35,7 +47,7 @@ public class Controller {
         return order;
     }
 
-    @PutMapping(path = "{idOrder}/{idStation}")
+    @PutMapping(path = {"/user/{idOrder}/{idStation}", "/admin/{idOrder}/{idStation}"})
     Order updateOrder(@PathVariable long idOrder, @PathVariable long idStation, @RequestBody Order order){
         Stations stations = stationsService.getStationById(idStation);
         Order found = orderService.getOrderById(idOrder);
