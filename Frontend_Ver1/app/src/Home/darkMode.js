fb572efd7bc98543ed66f480bc55843b0720ace1
hai-fpt <96479650+hai-fpt@users.noreply.github.com>
2022-08-18 14:52:07 +0700

    Update darkMode.js

--- Frontend_Ver1/app/src/Home/darkMode.js
+++ Frontend_Ver1/app/src/Home/darkMode.js
@@ -13,7 +13,11 @@ const storedTheme = localStorage.getItem("theme");
 
 const prefersDark =
     window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
-
+const defaultDark =
+    storedTheme === "dark" || (storedTheme === null && prefersDark);
+if (defaultDark) {
+    setDark();
+}
 // const toggleTheme = (e) => {
 //     if (e.target.checked) {
 //         setDark();
@@ -35,9 +39,9 @@ const darkMode = () => {
     return (
         <>
             <div className={'toggle-theme-wrapper'}>
-                <span>☀</span>
+                <span>☀️</span>
                 <label className={'toggle-theme'} htmlFor={'checkbox'}>
-                    <input type={"checkbox"} id={'checkbox'} onChange={toggleTheme}/>
+                    <input type={"checkbox"} id={'checkbox'} onChange={toggleTheme} defaultChecked={defaultDark}/>
                     <div className={'slider round'}></div>
                 </label>
                 <span>🌒</span>
