2a198055199d220360c10487a6eefcf52ad4ef6f
hai-fpt <96479650+hai-fpt@users.noreply.github.com>
2022-08-18 11:59:39 +0700

    123

--- Frontend_Ver1/app/src/App.js
+++ Frontend_Ver1/app/src/App.js
@@ -57,6 +57,7 @@ function App() {
                         <Route path={'/admin/user/'}>
                             {/*<Route index element={<AdminUser/>}/>*/}
                             <Route path={'/admin/user/:cardNum'} element={<AdminUser/>}/>
+                            <Route path={'/admin/user/topup'} element = {<Topup/>}/>
                         </Route>
                     </Routes>
 
