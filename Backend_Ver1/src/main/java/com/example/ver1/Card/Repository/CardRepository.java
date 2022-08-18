2f289013bdbbf9b18b6668cc8e9d603fdd7f057a
hainv198 <hainvth161197@gmail.com>
2022-08-18 11:46:56 +0700

    561

--- Backend_Ver1/src/main/java/com/example/ver1/Card/Repository/CardRepository.java
+++ Backend_Ver1/src/main/java/com/example/ver1/Card/Repository/CardRepository.java
@@ -1,5 +1,6 @@
 package com.example.ver1.Card.Repository;
 import com.example.ver1.Card.Model.Card;
+import com.example.ver1.Customer.Model.Customer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,5 +15,6 @@ public interface CardRepository extends JpaRepository<Card,Long> {
     Optional<Card> findCardByCardNum(String cardNum);
 
     Boolean existsCardByCardNum(String username);
+
 
 }
