c10ee89ac8e46bdfea183abf104bd10dce8a84bb
Luyen Dao Dinh <dinhluyen45@gmail.com>
2022-09-12 09:33:14 +0700

    Update SecurityConfig.java

--- Backend_Ver1/src/main/java/com/example/ver1/Security/SecurityConfig.java
+++ Backend_Ver1/src/main/java/com/example/ver1/Security/SecurityConfig.java
@@ -89,8 +89,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //cities controller
         http.authorizeRequests().antMatchers(GET, "/api/v1/cities").hasAnyAuthority("user");
 
-<<<<<<< Updated upstream
-=======
+
         //orders controller
         http.authorizeRequests().antMatchers( "/orders").hasAnyAuthority("admin");
         http.authorizeRequests().antMatchers( "/orders/**").hasAnyAuthority("admin");
@@ -98,7 +97,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers( POST,"/orders").permitAll();
         http.authorizeRequests().antMatchers( PUT,"/user?**").permitAll();
         http.authorizeRequests().antMatchers( PUT,"/user?**").hasAnyAuthority("user");
->>>>>>> Stashed changes
 
         http.authorizeRequests().antMatchers(GET, "/api/*").hasAnyAuthority("user");
         http.authorizeRequests().antMatchers("/swagger-ui.html").permitAll();
