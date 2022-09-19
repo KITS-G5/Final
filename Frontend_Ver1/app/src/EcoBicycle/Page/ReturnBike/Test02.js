571b8f89c12f49ca49de9dda6ce67201adb09bba
hai-fpt <hainvhth2108031@fpt.edu.vn>
2022-09-19 14:48:51 +0700

    test

--- Frontend_Ver1/app/src/EcoBicycle/Page/ReturnBike/Test02.js
+++ Frontend_Ver1/app/src/EcoBicycle/Page/ReturnBike/Test02.js
@@ -16,7 +16,8 @@ export default function ReturnBikeTest() {
 
 
     useEffect(() => {
-        let url = 'http://localhost:8080/api/v1/stations/'
+        // let url = '/api/v1/stations/'
+        let url = "http://projectfinaltest-env.eba-vh2mysap.ap-northeast-1.elasticbeanstalk.com/api/v1/stations/"
         fetch(url)
             .then(res => res.json())
             .then(res => setStation(res))
